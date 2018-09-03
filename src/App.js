61b0e9be984208f6b8aff7aa6b0f636f78163d17
Krystian <kordaszewskikrystian@gmail.com>
2018-09-03 14:51:08 +0200

    Add component Oferta

--- src/App.js
+++ src/App.js
@@ -13,7 +13,51 @@ const Home = () => (
 
 const About = () => (
   <div>
-    About
+    <div class="bg-color-jasny">
+				<div id="uslugi" class="container text-center">
+					<div class="row">
+						<div class="col-sm-4 text-left">
+							<h2>Projekt jest cichym ambasadorem Twojej marki</h2>
+							<hr class="line-blue"/>
+							<p>Jesteśmy zespołem pełnym pasji, kreatywności i posiadającym narzędzia, odpowiadające potrzebom naszych Klientów. Uzupełniamy
+								się umiejętnościami, dlatego nie ma dla nas rzeczy niemożliwych.</p>
+							<p class="margin-text-servises">Ważne jest dla nas zrozumienie celów, planów oraz otoczenia. Słuchamy i wdrażamy skuteczne strategie, tworząc unikalne
+								projekty.</p>
+							<div class="text-center">
+								<a href="portfolio.php">
+									<button type="button" class="btn btn-primary">Więcej</button>
+								</a>
+							</div>
+						</div>
+						<div class="col-sm-7 col-sm-offset-1">
+							<div class="row">
+								<div id="test11" class="col-sm-6 servises">
+									<i class="fa fa-users fa-4x text-primary sr1" aria-hidden="true"></i>
+									<h3>Social media</h3>
+									<p>kreujemy wizerunek zgodny z marką, angażujemy, monitorujemy i koordynujemy wszelkie działania</p>
+								</div>
+								<div class="col-sm-6 servises">
+									<i class="fa fa-lightbulb-o fa-4x text-primary sr1" aria-hidden="true"></i>
+									<h3>Kampanie reklamowe</h3>
+									<p>opracowywanie strategii kampanii i jej optymalizacja to nieodłączny element marketingu</p>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-6 servises">
+									<i class="fa fa-camera fa-4x text-primary sr12" aria-hidden="true"></i>
+									<h3>video & foto</h3>
+									<p>fotografujemy, nagrywamy i edytujemy, by pokazać atuty Twojego produktu, usługi i całej marki</p>
+								</div>
+								<div class="col-sm-6 servises">
+									<i class="fa fa-laptop fa-4x text-primary sr12" aria-hidden="true"></i>
+									<h3>Strony www</h3>
+									<p>zadbamy by Twoja strona internetowa była responsywna, intuicyjna i zatrzymywała klientów na dłużej</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
   </div>
 )
 
