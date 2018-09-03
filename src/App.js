c1530e25834574447898db8e5714be0c600e6cb2
Krystian <kordaszewskikrystian@gmail.com>
2018-09-03 13:32:41 +0200

    Add router to app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,81 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+
+
+const Home = () => (
+  <div>
+    Home
+  </div>
+)
+
+const About = () => (
+  <div>
+    About
+  </div>
+)
+
+const Code = () => (
+  <div>
+    Code
+  </div>
+)
+
+const Contact = () => (
+  <div>
+    Contact
+  </div>
+)
+
+const info = () => (
+  <div>
+    info
+  </div>
+)
+
+const MainMenu = () => {
+  return (
+    <div>
+      <Link to="/">
+        <button>home</button>
+      </Link>
+      <Link to="/about">
+        <button>About</button>
+      </Link>
+      <Link to="/code">
+        <button>code</button>
+      </Link>
+      <Link to="/contact">
+        <button>contact</button>
+      </Link>
+      <Link to="/info">
+        <button>info</button>
+      </Link>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            <MainMenu/>
+          </header>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/code" component={Code} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/presence" component={info} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
